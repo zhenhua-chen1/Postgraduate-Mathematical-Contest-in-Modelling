515f961c72f8b559da8ca6d23544b3f8441809b1
zhenhua-chen1 <41711519+zhenhua-chen1@users.noreply.github.com>
2022-09-30 11:09:11 +0800

    2021年F题第1问

--- 2021年F题第1问/B/output_flight.m
+++ 2021年F题第1问/B/output_flight.m
@@ -1,7 +1,7 @@
 function output_flight(path_all1,path_all2,I2)
     [a1,b]=size(path_all1);
     [a2,b]=size(path_all2);
-    rawA = readcell('机组排班Data B-Flight1.xlsx');
+    rawA = readcell('机组排班Data B-Flight.xlsx');
     rawB = readcell('机组排班Data B-Crew.csv');
     twt={'员工号','航段次序','航班号','出发日期','出发时间','出发机场','到达日期','到达时间','到达机场','实际资格'};
     twt2={'航班号','出发日期','出发时间','出发机场','到达日期','到达时间','到达机场','最低配置要求'};
@@ -26,7 +26,7 @@ function output_flight(path_all1,path_all2,I2)
           temp_path=path_all1(p,:);
           temp_path(find(path_all1(p,:)==0))=[];
           for i=1:length(temp_path)
-              path=temp_path(i);
+              path=temp_path(i)+1;
               twt=[twt;[Captain,i,rawA(path,1:7),'C']];
               twt=[twt;[FirstOfficer,i,rawA(path,1:7),'F']];
           end
@@ -51,13 +51,13 @@ function output_flight(path_all1,path_all2,I2)
           temp_path(find(path_all2(p,:)==0))=[];
           for i=1:length(temp_path)
               path=temp_path(i);
-              twt=[twt;[Captain,i,rawA(path,1:7),'C']];
-              twt=[twt;[FirstOfficer,i,rawA(path,1:7),'F']];
+              twt=[twt;[Captain,i,rawA(path,2:8),'C']];
+              twt=[twt;[FirstOfficer,i,rawA(path,2:8),'F']];
           end
     end 
     for i=I2
-       twt2=[twt2;rawA(i,1:8)];
+       twt2=[twt2;rawA(i,2:9)];
     end
-     writecell(twt,'CrewRosters.csv')
-     writecell(twt2,'UncoveredFlights.csv')
+     writecell(twt,'UncoveredFlights.xlsx')
+     writecell(twt2,'CrewRosters.xlsx')
 end
